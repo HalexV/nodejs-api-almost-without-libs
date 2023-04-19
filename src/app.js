3c8a78666887ac0165e911e28dca0b2d3d8fccc7
Halex Viotto Gomes <halexx002@gmail.com>
2023-04-19 10:34:27 -0400

    feat: create post tasks

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import http from "node:http";
+import parseJson from "./middlewares/parseJson.js";
+import Task from "./entities/Task.js";
+import database from "./database/database.js";
 
-const app = http.createServer((req, res) => {
-  res.end("OK");
+const app = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await parseJson(req, res);
+
+  if (method === "POST" && url === "/tasks") {
+    const { title, description } = req.body;
+
+    const task = new Task({ title, description });
+
+    const result = await database.insert("tasks", task);
+
+    return res.end(JSON.stringify(result));
+  }
+
+  return res.end();
 });
 
 export default app;
